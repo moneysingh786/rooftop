8528ed693e4b0a56fa6c2def69eb125bab776638
Yogesh Maggu <yogeshmaggu786@gmail.com>
2019-04-19 14:03:53 +0530

    updated views

--- devteam/tigrillo/views.py
+++ devteam/tigrillo/views.py
@@ -1,11 +1,25 @@
 from django.views.generic import TemplateView, ListView
+from django.core.paginator import EmptyPage, Paginator
+from django.http import Http404
 from .models import Orders,OrderItem
 from django.http import JsonResponse
 # class OrderView(TemplateView):
 #     template_name = "iccinfo.html"
 
 
+class SafePaginator(Paginator):
+    def validate_number(self, number):
+        try:
+            return super(SafePaginator, self).validate_number(number)
+        except EmptyPage:
+            if number > 1:
+                return self.num_pages
+            else:
+                raise
+
+
 class OrderView(ListView):
+    paginator_class = SafePaginator
     model = OrderItem
     template_name = 'iccinfo.html'  # Default: <app_label>/<model_name>_list.html
     context_object_name = 'orders'  # Default: object_list
@@ -19,7 +33,7 @@ def searchorder(request):
         if search_text is not None and search_text != u"":
             search_text = request.GET['searchtext']
             print(search_text)
-            order_status = OrderItem.objects.all().filter(ItemDescription__contains=search_text)[:10]
+            order_status = OrderItem.objects.all().filter(ItemDescription__icontains=search_text)[:10]
             order_list = []
             for os in order_status:
                 order_details = {}
